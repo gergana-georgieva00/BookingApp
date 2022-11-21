c5e7e01c3f88db0eed493057b329894381b234e8
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-21 21:49:48 +0200

    C# OOP Retake Exam – 22 Aug 2022

---
+++ Repositories/RoomRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Repositories
+{
+    public class RoomRepository : IRepository<IRoom>
+    {
+        private List<IRoom> rooms;
+
+        public RoomRepository()
+        {
+            this.rooms = new List<IRoom>();
+        }
+        
+        public void AddNew(IRoom model)
+        {
+            rooms.Add(model);
+        }
+
+        public IReadOnlyCollection<IRoom> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRoom Select(string criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
