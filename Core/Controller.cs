d68eee1c56a1dd8f01eb898da6cdb23376ea179f
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-22 00:29:56 +0200

    C# OOP Retake Exam – 22 Aug 2022

---
+++ Core/Controller.cs
@@ -0,0 +1,43 @@
+﻿using BookingApp.Core.Contracts;
+using BookingApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Core
+{
+    public class Controller : IController
+    {
+        private HotelRepository hotels;
+
+        public Controller()
+        {
+
+        }
+
+        public string AddHotel(string hotelName, int category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BookAvailableRoom(int adults, int children, int duration, int category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HotelReport(string hotelName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetRoomPrices(string hotelName, string roomTypeName, double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UploadRoomTypes(string hotelName, string roomTypeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
