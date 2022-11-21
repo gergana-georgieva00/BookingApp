9a52a39bf6a1b416353d0d1aaf7cc53a42f0d787
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-21 20:09:36 +0200

    C# OOP Exam – Hotel Booking Application

---
+++ Models/Rooms/Room.cs
@@ -0,0 +1,44 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms
+{
+    public class Room : IRoom
+    {
+        private int bedCapacity;
+        private double pricePerNight;
+
+        public Room()
+        {
+
+        }
+
+        public int BedCapacity 
+        {
+            get => this.bedCapacity;
+            private set
+            {
+
+            }
+        }
+
+        public double PricePerNight
+        {
+            get => this.pricePerNight;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(Utilities.Messages.ExceptionMessages.PricePerNightNegative);
+                }
+            }
+        }
+
+        public void SetPrice(double price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
