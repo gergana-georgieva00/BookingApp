e06273cd3c066f8c92f3aa0d8d45880063f208ba
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-21 20:22:37 +0200

    C# OOP Exam – Hotel Booking Application

---
+++ Models/Bookings/Booking.cs
@@ -0,0 +1,77 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Rooms.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Bookings
+{
+    public class Booking : IBooking
+    {
+        private int residenceDuration;
+        private int adultsCount;
+        private int childrenCount;
+        private IRoom room;
+        private int bookingNumber;
+
+        public Booking(IRoom room, int residenceDuration, int adultsCount, int childrenCount, int bookingNumber)
+        {
+            this.room = room;
+            this.residenceDuration = residenceDuration;
+            this.adultsCount = adultsCount;
+            this.childrenCount = childrenCount;
+            this.bookingNumber = bookingNumber;
+        }
+
+        public IRoom Room => room;
+
+        public int ResidenceDuration 
+        {
+            get => this.residenceDuration;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(Utilities.Messages.ExceptionMessages.DurationZeroOrLess);
+                }
+
+                this.residenceDuration = value;
+            }
+        }
+
+        public int AdultsCount
+        {
+            get => this.adultsCount;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(Utilities.Messages.ExceptionMessages.AdultsZeroOrLess);
+                }
+
+                this.adultsCount = value;
+            }
+        }
+
+        public int ChildrenCount
+        {
+            get => this.childrenCount;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(Utilities.Messages.ExceptionMessages.ChildrenNegative);
+                }
+
+                this.childrenCount = value;
+            }
+        }
+
+        public int BookingNumber => this.bookingNumber;
+
+        public string BookingSummary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
